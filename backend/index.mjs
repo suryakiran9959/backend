4907533312ca567bb891872f5d0333ba38045be8
suryakiran9959 <surya.mail.net@gmail.com>
2024-10-18 23:04:35 +0530

    s

--- backend/index.mjs
+++ backend/index.mjs
@@ -9,22 +9,33 @@ app.get("/",(request,response)=>{
 })
 
 let connection = mysql2.createConnection({
-    
+    host: "localhost",
+    user: "surya",
+    password: "965231",
+    database: "db"
+ });
+
+connection.connect((err)=>{
+    if(err){
+        console.log("err in database connection");
+    }else{
+        console.log("database connected");
+    }
 })
-
-
-
-
-
-
-
-
-
-
 
 app.listen(port,()=>{
     console.log(`server running from port ${port}`);
 })
+
+// connection to database sql server 
+
+// write this in the sql workbench
+//create database db;
+// use db;
+// create user "surya"@"localhost" identified by "965231";
+// GRANT all privileges on db.* to "surya"@"localhost";
+
+
 
 
 
