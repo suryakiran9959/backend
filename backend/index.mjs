059dfff0ddaee936715c9144a3417bdf33b0ef63
suryakiran9959 <surya.mail.net@gmail.com>
2024-10-20 00:12:57 +0530

    s

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,34 +1,66 @@
 import express from "express";
-import mysql2 from "mysql2"
+import mysql2 from "mysql2";
+import cors from "cors"
 
-const app = express() 
-const port = 3000; 
+const app = express();
+const port = 9999;
+app.use(express.json());
+app.use(cors());
 
-app.get("/",(request,response)=>{
-    response.send("hi im from backend")
-})
 
 
 let connection = mysql2.createConnection({
-    host: "localhost",
-    user: "surya",
-    password: "965231",
-    database: "db"
- });
+  host: "localhost",
+  user: "surya",
+  password: "965231",
+  database: "db"
+});
 
-connection.connect((err)=>{
-    if(err){
-        console.log("err in database connection");
-    }else{
-        console.log("database connected");
+connection.connect((err) => {
+  if (err) {
+    console.log("err in database connection");
+  } else {
+    console.log("database connected");
+  }
+});
+
+app.get("/allusers", (request, response) => {
+  let query = "select * from surya";
+  connection.query(query, (err, result) => {
+    if (err) {
+      console.log("err in fetching users");
+      response.send("error in fetching users");
+    } else {
+      console.log("data fetched successfully");
+      response.json(result);
     }
-})
+  });
+});
 
-app.listen(port,()=>{
-    console.log(`server running from port ${port}`);
-})
+app.post("/adduser", (req, res) => {
+  let { name, password } = req.body;
+  console.log(req.body);
+  
+  // No need to include 'id' in the query, as it will auto-increment
+  let query = "INSERT INTO surya(name, password) VALUES (?, ?)";
+  
+  connection.query(query, [name, password], (err, result) => {
+    if (err) {
+      console.log("error in adding data");
+      res.send("error in adding data");
+    } else {
+      console.log("data inserted");
+      res.send("data inserted successfully");
+    }
+  });
+});
 
 
 
 
 
+
+
+app.listen(port, () => {
+  console.log(`server running from port ${port}`);
+});
